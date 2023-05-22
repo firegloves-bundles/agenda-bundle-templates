176bbddbef22c7e259d8b44e075bc6daabc08a2c
firegloves <lucorset@gmail.com>
2023-05-22 15:44:01 +0200

    ENG-4873 init bundle and spring boot microservice

---
+++ microservices/springboot-agenda/src/main/java/com/entando/springbootagenda/SpringbootAgendaApplication.java
@@ -0,0 +1,13 @@
+package com.entando.springbootagenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAgendaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAgendaApplication.class, args);
+    }
+
+}
---
+++ microservices/springboot-agenda/src/test/java/com/entando/springbootagenda/SpringbootAgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entando.springbootagenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAgendaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
