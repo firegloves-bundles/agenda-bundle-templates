0bae8f1ff15a6a4436b7125df7e6a597ec79356d
Anthony Viard <anth.viard@gmail.com>
2023-05-30 18:08:48 +0200

    [ENG-4874] change the contact mapper IT to unit tests

--- microservices/springboot-agenda/src/test/java/com/entando/springbootagenda/service/mapper/ContactMapperTest.java
+++ microservices/springboot-agenda/src/test/java/com/entando/springbootagenda/service/mapper/ContactMapperTest.java
@@ -6,18 +6,10 @@ import static org.junit.Assert.assertNotNull;
 import com.entando.springbootagenda.model.entity.ContactEntity;
 import com.entando.springbootagenda.model.record.ContactRecord;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = ContactMapper.class)
-public class ContactMapperIT {
+public class ContactMapperTest {
 
-    @Autowired
-    private ContactMapper mapper;
-
+    private final ContactMapper mapper = new ContactMapper();
 
     @Test
     public void testToEntityFull() {
