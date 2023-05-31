790a503f7984b385c840fdebf2b011c1e85a52e4
firegloves <lucorset@gmail.com>
2023-05-31 17:49:57 +0200

    ENG-4920 sonar analysis for mfes

--- microfrontends/react-widget-agenda/craco.config.js
+++ microfrontends/react-widget-agenda/craco.config.js
@@ -1,31 +1,22 @@
+const path = require('path');
+
 module.exports = {
     webpack: {
-      plugins: {
-        remove: ['MiniCssExtractPlugin']
-      },
       configure: (webpackConfig, { env, paths }) => {
-        webpackConfig.module.rules[1].oneOf[5].use.shift()
-        webpackConfig.module.rules[1].oneOf[6].use.shift()
-        webpackConfig.module.rules[1].oneOf[7].use.shift()
-        webpackConfig.module.rules[1].oneOf[8].use.shift()
+        const appPackageJson = require(paths.appPackageJson);
+        const entry = paths.appIndexJs;
+        const output = {
+            filename: `static/js/${appPackageJson.name}-${appPackageJson.version}.js`,
+            library: {
+                type: 'umd',
+                name: 'react-widget-agenda',
+            },
+            path: path.resolve(__dirname, 'build')
+        };
+        paths.appBuild = output.path;
+        webpackConfig.entry = entry;
+        webpackConfig.output = output;
 
-        const styleLoaderConf = {
-            loader: "style-loader",
-            options: {
-                injectType: "lazyStyleTag",
-                insert: function insertIntoTarget(element, options) {
-                  var parent = options.target || document.head;
-  
-                  parent.appendChild(element);
-                },
-            }
-        }
-
-        webpackConfig.module.rules[1].oneOf[5].use.unshift(styleLoaderConf)
-        webpackConfig.module.rules[1].oneOf[6].use.unshift(styleLoaderConf)
-        webpackConfig.module.rules[1].oneOf[7].use.unshift(styleLoaderConf)
-        webpackConfig.module.rules[1].oneOf[8].use.unshift(styleLoaderConf)
- 
         return webpackConfig;
       },
     },
