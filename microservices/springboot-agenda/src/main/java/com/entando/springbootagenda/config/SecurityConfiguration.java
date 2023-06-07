c3eea5148592e53450dbc32e26bde97e216f49d4
Matteo E. Minnai <m.minnai@entando.com>
2023-06-07 10:13:28 +0200

    Excluded CSRF from Sonar inspection

--- microservices/springboot-agenda/src/main/java/com/entando/springbootagenda/config/SecurityConfiguration.java
+++ microservices/springboot-agenda/src/main/java/com/entando/springbootagenda/config/SecurityConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @Configuration
@@ -16,7 +15,8 @@ public class SecurityConfiguration {
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
         return http
-                .csrf(cors -> cors.disable())
+                // nosonar
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers("/api/**").permitAll()
                         .requestMatchers("/swagger-ui/**").permitAll()
