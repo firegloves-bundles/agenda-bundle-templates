40754b3ce90091c00993feb23d12e6fff02bd10f
Alessio Pintus <alessio.pintus@gmail.com>
2023-06-28 11:11:04 +0200

    ENG-4968 Update keycloak login

--- microfrontends/vue-widget-agenda/src/main.js
+++ microfrontends/vue-widget-agenda/src/main.js
@@ -4,16 +4,28 @@ import Keycloak from "keycloak-js";
 
 const WidgetCustomElement = defineCustomElement(VueWidgetAgenda);
 
-if (import.meta.env.PROD) {
-    customElements.define("vue-widget-agenda", WidgetCustomElement);
-} else {
-    let initOptions = {
-        url: import.meta.env.VITE_KEYCLOAK_URL,
-        realm: import.meta.env.VITE_KEYCLOAK_REALM,
-        clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID,
-        onLoad: import.meta.env.PROD ? 'check-sso ' : 'login-required'
+    let initOptions;
+
+    console.log(`--> main-silentCheckSsoRedirectUri: `, `${window.location.origin}/entando-de-app/resources/static/silent-check-sso.html`)
+
+    if (import.meta.env.PROD) {
+        initOptions = {
+            onLoad: 'check-sso',
+            silentCheckSsoRedirectUri: `${window.location.origin}/entando-de-app/resources/static/silent-check-sso.html`,
+            promiseType: 'native',
+            enableLogging: true
+        }
+    } else {
+        initOptions = {
+            url: import.meta.env.KEYCLOAK_URL,
+            realm: import.meta.env.KEYCLOAK_REALM,
+            clientId: import.meta.env.KEYCLOAK_CLIENT_ID,
+            onLoad: 'login-required'
+        }
     }
+
     let keycloak = Keycloak(initOptions);
+
     keycloak.init({onLoad: initOptions.onLoad}).then((auth) => {
         if (!auth) {
             window.location.reload();
@@ -25,25 +37,20 @@ if (import.meta.env.PROD) {
             };
             customElements.define("vue-widget-agenda", WidgetCustomElement);
         }
-        setInterval(() => {
-            keycloak.updateToken(70).then((refreshed) => {
-                if (refreshed) {
-                    console.debug('Token refreshed' + refreshed);
-                } else {
-                    console.warn('Token not refreshed, valid for '
-                        + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
-                }
-            }).catch(() => {
-                console.error('Failed to refresh token');
-            });
-        }, 60000)
+        if (import.meta.env.DEV) {
+                setInterval(() => {
+                keycloak.updateToken(70).then((refreshed) => {
+                    if (refreshed) {
+                        console.debug('Token refreshed' + refreshed);
+                    } else {
+                        console.warn('Token not refreshed, valid for '
+                            + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
+                    }
+                }).catch(() => {
+                    console.error('Failed to refresh token');
+                });
+            }, 60000)
+        }
     }).catch(() => {
         console.error("Authenticated Failed");
     });
-}
-
-
-
-
-
-
